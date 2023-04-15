6945c0395b7f2bdc326bb2d8bceeeef63ab429ce
devinoue <itisagoodcat@gmail.com>
2023-04-15 21:12:03 +0900

    test

---
+++ src/components/Icons/MinusIcon.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export function MinusIcon() {
+  return (
+    <svg fill='none' height='2' viewBox='0 0 13 2' width='13' xmlns='http://www.w3.org/2000/svg'>
+      <path
+        clipRule='evenodd'
+        d='M0.416992 1C0.416992 0.585786 0.752779 0.25 1.16699 0.25H11.8337C12.2479 0.25 12.5837 0.585786 12.5837 1C12.5837 1.41421 12.2479 1.75 11.8337 1.75H1.16699C0.752779 1.75 0.416992 1.41421 0.416992 1Z'
+        fill='black'
+        fillRule='evenodd'
+      />
+    </svg>
+  )
+}
